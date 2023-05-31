f26c424de3c08c23ef8170a4a6524c352934fb1a
Gabriel Tu <gabriel8215@hotmail.com>
2023-05-30 19:16:34 -0700

    initial commit

---
+++ socket.ts
@@ -0,0 +1,13 @@
+import { Server } from "socket.io"
+
+
+export default (httpServer) => {
+    const io = new Server(httpServer, {
+        cors: {
+            origin: "*", 
+        },
+    });
+
+    
+    return io; 
+};
